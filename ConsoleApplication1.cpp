64847359f1554bdf269ed8f2c884f28b9c9ac28d
Daniel Vazquez <fienbeh@gmail.com>
2023-11-28 13:27:11 -0600

    cxc

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -15,7 +15,7 @@ int main()
     double result = 0.0;
     char oper = '+';
 
-    cout << " Console Application" << endl << endl;
+    cout << " Console app" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 
     Calculator c;
