c44918fb9874c02d6aaac4faf49060f76dcd1c95
Daniel Vazquez <fienbeh@gmail.com>
2023-11-28 13:26:14 -0600

    cc

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -15,7 +15,7 @@ int main()
     double result = 0.0;
     char oper = '+';
 
-    cout << "Calculator Console Application" << endl << endl;
+    cout << " Console Application" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 
     Calculator c;
